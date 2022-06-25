fadcb147f4afe16a205cf853bec2b91148bfc6e4
iwamizawa-software <iwamizawasoftware@icloud.com>
2022-06-26 04:17:19 +0900

    取り急ぎ

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # astral-omega-extension
-もなちゃと☆ω用userscript
+
+[もなちゃと☆ω用](https://monachat.life/)userscript
+
+## ライセンス
+
+[![CC0](https://licensebuttons.net/p/zero/1.0/88x31.png) ](https://creativecommons.org/publicdomain/zero/1.0/deed.ja)
+
+## インストール
+
+1. [astral-omega-extension.user.js](https://raw.githubusercontent.com/iwamizawa-software/astral-omega-extension/main/astral-omega-extension.user.js)
+2. 自分で編集
+
---
+++ astral-omega-extension.user.js
@@ -0,0 +1,100 @@
+// ==UserScript==
+// @name     astral-omega-extension
+// @version  0
+// @grant    none
+// @run-at   document-start
+// @match    https://monachat.life/*
+// ==/UserScript==
+
+var inject = function () {
+
+  var mentionPattern = /[セせ][ンん][ゔヴべ]|senn?[bv]|🍘/i;
+  var replyMsg = 'ｎ';
+
+  var pngCache = {};
+  var SVG2PNG = async function (url) {
+    var callback;
+    if (url.slice(-3) === 'png')
+      return url;
+    if (pngCache[url])
+      return pngCache[url];
+    try {
+      var canvas = document.createElement('canvas');
+      var ctx = canvas.getContext('2d');
+      var img = new Image();
+      img.onload = function () {
+        canvas.width = img.width;
+        canvas.height = canvas.height;
+        ctx.drawImage(img, 0, 0);
+        callback(pngCache[url] = canvas.toDataURL('image/png'));
+      };
+      img.src = 'data:image/svg+xml,' + encodeURIComponent(await (await fetch(url)).text());
+    } catch (err) {
+      return url;
+    }
+    return {then:c=>callback=c};
+  };
+  var mention;
+  var mentionNotification = async function (user, cmt, onclick) {
+    mention = new Notification(user.name, {
+      // ChromeはNotification.iconにSVGを指定できない
+      icon: await SVG2PNG('https://monachat.life/img/svg/' + user.type + '.svg'),
+      tag: 'mention',
+      body: cmt,
+      requireInteraction: true
+    });
+    mention.onclick = onclick;
+  };
+  addEventListener('focus', function () {
+    if (mention)
+      mention.close();
+  });
+  Notification.requestPermission();
+
+  var WebSocket = window.WebSocket;
+  window.WebSocket = function (url, protocols) {
+    var socket = new WebSocket(url, protocols);
+    var token, myId, users = {};
+    socket.addEventListener('message', event => {
+      var json = event.data.replace(/^\d+/, '');
+      if (!json)
+        return;
+      try {
+        var data = JSON.parse(json);
+      } catch (err) {
+        return;
+      }
+      switch (data[0]) {
+        case 'AUTH':
+          token = data[1].token;
+          myId = data[1].id;
+          break;
+        case 'USER':
+          users = {};
+          data[1].forEach(u => users[u.id] = u);
+          break;
+        case 'ENTER':
+          users[data[1].id] = data[1];
+          break;
+        case 'EXIT':
+          delete users[data[1].id];
+          break;
+        case 'COM':
+          if (data[1].id === myId)
+            return;
+          if (!document.hasFocus() && mentionPattern && mentionPattern.test(data[1].cmt))
+            mentionNotification(users[data[1].id], data[1].cmt, () => {
+              if (replyMsg)
+                socket.send('42' + JSON.stringify(['COM', {token: token, cmt: replyMsg}]));
+              // Chromeはクリック時既定の動作がない
+              focus();
+            });
+          break;
+      }
+    });
+    return socket;
+  };
+  window.WebSocket.prototype = WebSocket.prototype;
+  window.WebSocket.__proto__ = WebSocket;
+};
+window.eval('(' + inject + ')()');
