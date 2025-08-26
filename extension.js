4da920f4891ef123f110d55f21f689bc79a1f165
iwamizawa-software <iwamizawasoftware@icloud.com>
2025-08-26 14:55:49 +0900

    stat undefined

--- extension.js
+++ extension.js
@@ -630,7 +630,7 @@ var inject = function () {
       attr.x = Math.max(+extensionConfig.minX, Math.min(+extensionConfig.maxX, attr.x));
     if ('y' in attr)
       attr.y = Math.max(+extensionConfig.minY, Math.min(+extensionConfig.maxY, attr.y));
-    if (attr && typeof attr.stat !== 'string')
+    if (attr?.stat !== undefined && typeof attr.stat !== 'string')
       attr.stat += '';
     Bot.send('SET', Object.assign({x, y, scl, stat}, attr));
     Object.assign(Bot.users[Bot.myId], attr);
