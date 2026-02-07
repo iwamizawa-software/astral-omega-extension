71787613f6c61eb7f96b9d059fbcb4307f7f9ab2
iwamizawa-software <iwamizawasoftware@icloud.com>
2026-02-08 00:54:08 +0900

    white background

--- extension.js
+++ extension.js
@@ -659,9 +659,10 @@ var inject = function () {
           styles.push(style);
           styles.push('');
         }
-        buf += '\n' + user.stat.replace(/[♠♥♣♦][A23456789⒑JQK](?:　|$)/g, card => {
+        styles.push('background-color:white;color:black');
+        buf += '\n%c' + user.stat.replace(/[♠♥♣♦][A23456789⒑JQK](?:　|$)/g, card => {
           if (currentNumber < 0 || card[0] === currentCard[0] || card[1] === currentCard[1]) {
-            styles.push('color:yellow', '');
+            styles.push('background-color:white;color:yellow', 'background-color:white;color:black');
             card = '%c' + card + '%c';
           }
           return card;
